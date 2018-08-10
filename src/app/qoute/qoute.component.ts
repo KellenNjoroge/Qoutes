4504e9557ea1440291b4464c5bc7154ee858dff1
KellenNjoroge <muthonkel@gmail.com>
2018-08-10 14:30:37 +0300

    made changes component qoute

--- src/app/qoute/qoute.component.ts
+++ src/app/qoute/qoute.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Qoute} from '../qoute';
 @Component({
   selector: 'app-qoute',
   templateUrl: './qoute.component.html',
   styleUrls: ['./qoute.component.css']
 })
 export class QouteComponent implements OnInit {
+
+  qoutes = [
+    new Qoute(1, 'You cannot follow somebody you know you should lead'),
+    new Qoute(2, 'but who fights for you'),
+    new Qoute(3, 'Love is never wasted for its value does not rest in its reprocity'),
+    new Qoute(4, ' Art is to console those who are broken by life'),
+   ];
 
   constructor() { }
 
