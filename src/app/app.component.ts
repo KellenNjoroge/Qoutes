fad1b904df8c9873fbaf4d839f8f82741edefa5e
KellenNjoroge <muthonkel@gmail.com>
2018-08-10 14:15:48 +0300

    created class Qoute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {Qoute} from './qoute';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Qoutes';
+  qoutes = [
+    new Qoute(1, 'You cannot follow somebody you know you should lead'),
+    new Qoute(2, 'but who fights for you'),
+    new Qoute(3, 'Love is never wasted for its value does not rest in its reprocity'),
+    new Qoute(4, ' Art is to console those who are broken by life'),
+   ];
 }
---
+++ src/app/qoute.ts
@@ -0,0 +1,3 @@
+export class Qoute {
+  constructor (public id: number, public name: string) {}
+}
