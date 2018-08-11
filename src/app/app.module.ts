4e3ad83153fc5821424e4cbf14e005541c2f0f7f
KellenNjoroge <muthonkel@gmail.com>
2018-08-11 14:06:20 +0300

    added qoute form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QouteComponent } from './qoute/qoute.component';
 import { QouteDetailsComponent } from './qoute-details/qoute-details.component';
+import { QouteFormComponent } from './qoute-form/qoute-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QouteComponent,
-    QouteDetailsComponent
+    QouteDetailsComponent,
+    QouteFormComponent
   ],
   imports: [
     BrowserModule
