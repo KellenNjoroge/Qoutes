939501825050d8e738678d3e48cc9d08be9f4127
KellenNjoroge <muthonkel@gmail.com>
2018-08-11 15:04:19 +0300

    two-way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { QouteComponent } from './qoute/qoute.component';
 import { QouteDetailsComponent } from './qoute-details/qoute-details.component';
@@ -14,7 +14,8 @@ import { QouteFormComponent } from './qoute-form/qoute-form.component';
     QouteFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/qoute-form/qoute-form.component.ts
+++ src/app/qoute-form/qoute-form.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Qoute} from '../qoute';
 @Component({
   selector: 'app-qoute-form',
   templateUrl: './qoute-form.component.html',
   styleUrls: ['./qoute-form.component.css']
 })
 export class QouteFormComponent implements OnInit {
-
+newQoute = new Qoute(0, '', '') ;
   constructor() { }
 
   ngOnInit() {
